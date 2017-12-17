12840a504582f943da5cbe100ffba650ad09c924
Mark Powalisz <markpowalisz@Marks-MacBook-Pro.local>
2017-12-17 02:48:15 -0500

    12/17 14:48

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,14 +1,18 @@
 // An array storing pre-determined horror icons.
 $(document).ready(function () {
 
-    var horrorIcon = ["Jason", "Chucky", "Freddy Krueger"];
-    newButton = "antelope"
+    var horrorIcon = ["jason", "chucky", "freddy krueger"];
 
-    // This is not what I want. This should technically be tied to the buttons generated dynamically, but this
-    // was okay for testing.
-    $("#submit").on("click", function () {
+
+    // When an "icon" button is clicked, it should run the AJAX call for the API
+    $(".icon").on("click", function () {
+
+        newButton = ("hamster");
         // AJAX Call for pulling data from GIPHY
-        var queryURL = "https://api.giphy.com/v1/gifs/trending?q=" + newButton + "&limit=20&api_key=YVYenRV6DNvcFzei71GxTtitudGD05R5";
+        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + newButton + "&limit=20&api_key=YVYenRV6DNvcFzei71GxTtitudGD05R5";
+        // Console log the QueryURL to insure it's correct ~ thanksJodi
+        console.log(queryURL);
+        // Console log the response to see the JSON format
 
         $.ajax({
             url: queryURL,
@@ -16,8 +20,6 @@ $(document).ready(function () {
             // I hereby promise I won't use the response until I have a response
         }).done(function (response) {
 
-            // Console log the QueryURL to insure it's correct ~thanksJodi
-            console.log(queryURL);
             // Console log the response to see the JSON format
             console.log(response);
             // Drill down one level into our response
@@ -28,7 +30,7 @@ $(document).ready(function () {
                 // Creates a div for the image and ratings
                 var resultsDiv = $("<div>");
 
-                // Creating a paragraph tag with the result item's rating
+                // Creating a paragraph tag with the result's rating
                 var p = $("<p>").text("Rating: " + results[i].rating);
 
                 // Creating and storing the img for HTML
@@ -37,35 +39,50 @@ $(document).ready(function () {
                 // Setting the src attribute of the image to a property pulled off the result item
                 resultsImage.attr("src", results[i].images.fixed_height.url);
 
-                // Appending the paragraph and image tag to the animalDiv
+                // Appending the paragraph and image tag to the resultsDiv
                 resultsDiv.append(p);
                 resultsDiv.append(resultsImage);
 
-                // Prependng the animalDiv to the HTML page in the "#gifs-appear-here" div
-                $("#dynBtns").prepend(resultsDiv);
+                // Prepends resultsDiv to HTML page in the "#gifs" div
+                $(".dynBtns").prepend(resultsDiv);
             }
 
         })
 
 
     })
+
     // Function for rendering buttons *Whoa*
     function renderButtons() {
         // Removes existing buttons
-        $("#dynButtons").empty();
-
+        $(".dynBtns").empty();
+        // For Loop for parsing horrorIcon array.... I've been avoiding these.
         for (var i = 0; i < horrorIcon.length; i++) {
 
-            var Icons = horrorIcon[i];
-            var Btn = ("<button>");
-            $(Btn).addClass("icon");
-            $(Btn).attr("data-name", horrorIcon[i]);
-            $("#dynBtns").append(Btn);
+            var Btn = $("<button>");
+            Btn.addClass("icon btn btn-primary");
+            Btn.attr("data-name", horrorIcon[i]);
+            Btn.text(horrorIcon[i]);
+            $(".dynBtns").append(Btn);
 
-            console.log(Icons);
+            console.log(horrorIcon[i]);
+
         }
     }
     // Calls function renderButtons()
     renderButtons()
 
+    // Submit will log the value enterered into the form and then renderButtons will create a button for it.
+    $("#submit").on("click", function (event) {
+        // event.preventDefault() prevents the form from trying to submit itself.
+        // We're using a form so that the user can hit enter instead of clicking the button if they want
+        event.preventDefault();
+        // This line will grab the text from the input box...if I can figure out what text to grab.
+        var icon = $("#horror").val().trim();
+        console.log(icon);
+        // The Icon from the form is added to the array horrorIcons
+        horrorIcon.push(icon);
+        // calls renderButtons again!!!
+        renderButtons();
+    });
 })
